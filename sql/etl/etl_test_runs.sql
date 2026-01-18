65f180d7ced850d6bb41a13bf65f35ea55e798f7
Surelux <slipgames123@gmail.com>
2026-01-18 15:58:50 -0600

    Update etl_test_runs.sql

--- sql/etl/etl_test_runs.sql
+++ sql/etl/etl_test_runs.sql
@@ -1,27 +1,15 @@
--- TEST: Insert sample data into staging tables
+EXEC etl.Load_DimVehicle;
+EXEC etl.Load_DimTime;
+EXEC etl.Load_DimLocation;
+EXEC etl.Load_FactTelemetry;
 
--- Clear staging tables first
-TRUNCATE TABLE staging.RawVehicle;
-TRUNCATE TABLE staging.RawTelemetry;
+SELECT 'DimVehicle', COUNT(*) FROM dw.DimVehicle
+UNION ALL
+SELECT 'DimTime', COUNT(*) FROM dw.DimTime
+UNION ALL
+SELECT 'DimLocation', COUNT(*) FROM dw.DimLocation
+UNION ALL
+SELECT 'FactTelemetry', COUNT(*) FROM dw.FactTelemetry;
 
--- Insert sample vehicle
-INSERT INTO staging.RawVehicle (VIN, Model, Make, Year, VehicleType, EngineType, BatteryType)
-VALUES
-('VIN123', 'Chiron', 'Bugatti', 2022, 'Hypercar', 'Quad-Turbo W16', 'N/A');
-
--- Insert sample telemetry
-INSERT INTO staging.RawTelemetry (VIN, TimestampUTC, Speed, BatteryTemp, MotorTemp, StateOfCharge, Country, State, City, Facility)
-VALUES
-('VIN123', '2024-01-01T12:00:00', 180.5, 75.2, 90.1, 100, 'USA', 'TN', 'Nashville', 'TestTrack');
-
--- Runing each ETL step in order
-EXEC etl.LoadDimVehicle;
-EXEC etl.LoadDimTime;
-EXEC etl.LoadFactTelemetry;
-
--- Runing final checks
-SELECT * FROM dw.DimVehicle;
-
-SELECT * FROM dw.DimTime;
-
-SELECT * FROM dw.FactTelemetry;
+SELECT TOP 1 VIN, TimestampUTC, City, State, Country
+FROM staging.StgTelemetry;
